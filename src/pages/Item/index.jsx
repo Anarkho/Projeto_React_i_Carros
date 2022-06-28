3764925bf993852dec4d79b01894822c7d4ebbe7
Alex Fernandes <alex.soares1414@gmail.com>
2022-06-28 09:04:13 -0300

    017 Estrutura item

--- src/pages/Item/index.jsx
+++ src/pages/Item/index.jsx
@@ -8,6 +8,9 @@ import LastPublication from '@/components/LastPublication'
 import Caminhos from '@/components/Caminhos'
 import iconCoracao from '@/assets/img/home/Item/icones/icon-coracao.png'
 import iconShared from '@/assets/img/home/Item/icones/icon-shared.png'
+import Video from '@/assets/img/home/Item/icones/video.png'
+
+import { mercedes } from '../../assets/img/home/top-offers/automoveis'
 
 const Item = () => {
   return (
@@ -23,7 +26,17 @@ const Item = () => {
           </div>
         </div>
         <div className="colunas">
-          <div className="coluna-left">left</div>
+          <div className="coluna-left">
+            <div className="carrosel">
+              <img className="img-principal" src={mercedes[0]} alt="mercedes" />
+            </div>
+            <div className="mini-imagens">
+              {mercedes.map((item) => (
+                <img src={item} alt="miniatura carros" />
+              ))}
+              <img src={Video} alt="video demostrativo" />
+            </div>
+          </div>
           <div className="coluna-right">right</div>
         </div>
         <LastPublication title="Você pode estar interessado" />
--- src/pages/Item/styles.js
+++ src/pages/Item/styles.js
@@ -35,8 +35,33 @@ export const Container = styled.div`
     justify-content: space-between;
 
     .coluna-left {
+      display: flex;
+      flex-direction: column;
       width: 55%;
       border: solid 1px blue;
+
+      .carrosel {
+        img {
+          border-radius: 1.2rem;
+          width: 100%;
+        }
+      }
+
+      .mini-imagens {
+        display: flex;
+        margin-top: 5px;
+        flex: none;
+        img {
+          margin: 0px 5px;
+          border-radius: 1rem;
+          width: 18%;
+          object-fit: cover;
+
+          &:hover {
+            border: solid 1px #fd5631;
+          }
+        }
+      }
     }
     .coluna-right {
       width: 45%;
